fba4c7a395f3fecf4119d186bca55aa296338ce9
daemord <daemord@gmail.com>
2020-12-03 10:57:25 +0000

    day03

--- main.py
+++ main.py
@@ -1,33 +1,78 @@
 import re
 
 
-def main():
 
-    f = open("/home/daemord/dev/AoC2020/input02.txt", "r")
-    fl = f.readlines()
-    passfound = 0
-    passfound2 = 0
+#Holding regex code for stuff
+
+
+# rex = re.compile('([0-9]+)-([0-9]+) (.): (.+)')
+# num1, num2, searchChar, passData = rex.search(x).groups()
+# num1 = int(num1)
+# num2 = int(num2)
+
+
+def data():
+
+    count = 0
+    dataArray = []
+    f = open("/home/daemord/dev/AoC2020/input03.txt", "r")
+    #f = open("/home/daemord/dev/AoC2020/test03.txt", "r")
+    fl = f.read().splitlines()
+    #put data into array
+
     for x in fl:
-        rex = re.compile('([0-9]+)-([0-9]+) (.): (.+)')
-        num1, num2, searchChar, passData = rex.search(x).groups()
-        num1 = int(num1)
-        num2 = int(num2)
-        if passData.count(searchChar) <= num2:
-            if passData.count(searchChar) >= num1:
-                passfound = passfound + 1
+        count += 1
+        holdArray= []
+        for y in range(0, len(x)):
+            if x[y] == ".":
+                tmp = 0
+            else:
+                tmp = 1
+            tmp=int(tmp)
+            holdArray.append(tmp)
+        dataArray.append(holdArray)
 
-        if passData[num1-1] == searchChar or passData[num2-1] == searchChar:
-            data1 = passData[num1-1]
-            data2 = passData[num2-1]
-            data3 = data1 + data2
-            print(data3)
-            countString = data3.count(searchChar)
-            print(searchChar)
-            print(countString)
-            if countString == 1:
-                passfound2 = passfound2 + 1
-    print(passfound)
-    print(passfound2)
+    return(dataArray),count,len(x)
 
-if __name__== "__main__":
-  main()
+def returnData(xcoord,ycoord,inputdata):
+    xcoord = xcoord - 1
+    ycoord = ycoord - 1
+    return inputdata[xcoord][ycoord]
+
+def treeshitt(treesx,treesy):
+    dataInput, sizex, sizey = data()
+    startcoordX = 1
+    startcoordY = 1
+    increasex = treesx
+    increasey = treesy
+    xCoOrd = startcoordX
+    yCoOrd = startcoordY
+    treeshit = 0
+
+    while xCoOrd < sizex + 1:
+        coordData = returnData(xCoOrd, yCoOrd, dataInput)
+        treeshit += coordData
+        xCoOrd += increasex
+        yCoOrd += increasey
+        if yCoOrd > sizey:
+            yCoOrd = yCoOrd - sizey
+
+    return treeshit
+
+print(11)
+ta=(treeshitt(1,1))
+print(ta)
+print(13)
+tb=(treeshitt(1,3))
+print(tb)
+print(15)
+tc=(treeshitt(1,5))
+print(tc)
+print(17)
+td=(treeshitt(1,7))
+print(td)
+print(21)
+te=(treeshitt(2,1))
+print(te)
+answer=ta*tb*tc*td*te
+print(answer)
