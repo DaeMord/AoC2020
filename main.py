26155b87723603f6b687871803352bbb4a4b3c1b
daemord <daemord@gmail.com>
2020-12-04 13:16:43 +0000

    Day 04

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import re
 
 #Holding regex code for stuff
 
+guess = 232
 
+#bigregex = re.compile('(byr:)(\S*)(ecl:)(\S*)')
+byrtest = re.compile('(byr:)(\S*)')
+iyrtest = re.compile('(iyr:)(\S*)')
+eyrtest = re.compile('(eyr:)(\S*)')
+hgttest = re.compile('(hgt:)(\S*)')
+hcltest = re.compile('(hcl:)(\S*)')
+ecltest = re.compile('(ecl:)(\S*)')
+pidtest = re.compile('(pid:)(\S*)')
+cidtest = re.compile('(cid:)(\S*)')
 # rex = re.compile('([0-9]+)-([0-9]+) (.): (.+)')
 # num1, num2, searchChar, passData = rex.search(x).groups()
 # num1 = int(num1)
@@ -15,64 +25,120 @@ def data():
 
     count = 0
     dataArray = []
-    f = open("/home/daemord/dev/AoC2020/input03.txt", "r")
-    #f = open("/home/daemord/dev/AoC2020/test03.txt", "r")
+    f = open("/home/daemord/dev/AoC2020/input04.txt", "r")
+    #f = open("/home/daemord/dev/AoC2020/input04test.txt", "r")
+    #f = open("/home/daemord/dev/AoC2020/input04test02.txt", "r")
+    #f = open("/home/daemord/dev/AoC2020/single04.txt", "r")
     fl = f.read().splitlines()
     #put data into array
-
+    byrData1 = ""
+    iyrData1 = ""
+    eyrData1 = ""
+    hgtData1 = ""
+    hclData1 = ""
+    eclData1 = ""
+    pidData1 = ""
+    cidData1 = ""
+    validTest = 0
+    validVerify = 0
+    dataentry = 0
     for x in fl:
-        count += 1
-        holdArray= []
-        for y in range(0, len(x)):
-            if x[y] == ".":
-                tmp = 0
-            else:
-                tmp = 1
-            tmp=int(tmp)
-            holdArray.append(tmp)
-        dataArray.append(holdArray)
+        if not x:
+            tmpArray = [byrData1, iyrData1, eyrData1, hgtData1, hclData1, eclData1, pidData1, cidData1, validTest]
+            if validTest == 7:
+                validVerify += 1
+            dataentry += 1
+            print(tmpArray)
+            byrData1 = ""
+            iyrData1 = ""
+            eyrData1 = ""
+            hgtData1 = ""
+            hclData1 = ""
+            eclData1 = ""
+            pidData1 = ""
+            cidData1 = ""
+            validTest = 0
+            dataArray.append(tmpArray)
 
-    return(dataArray),count,len(x)
+        byrData = byrtest.search(x)
+        if byrData:
+            byrData1 = byrData.groups()[1]
+            byrData1 = int(byrData1)
+            if 1920 <= byrData1 <= 2002:
+                validTest += 1
+            #print(byrData1)
+        iyrData = iyrtest.search(x)
+        if iyrData:
+            iyrData1 = iyrData.groups()[1]
+            iyrData1 = int(iyrData1)
+            if 2010 <= iyrData1 <= 2020:
+                validTest += 1
+            #print(iyrData)
+        eyrData = eyrtest.search(x)
+        if eyrData:
+            eyrData1 = eyrData.groups()[1]
+            eyrData1 = int(eyrData1)
+            if 2020 <= eyrData1 <= 2030:
+                validTest += 1
+            #print(eyrData)
+        hgtData = hgttest.search(x)
+        if hgtData:
+            hgtData1 = hgtData.groups()[1]
+            height, cmin = re.search('([0-9]+)(\S*)',hgtData1).groups()
+            height = int(height)
+            if cmin == "cm":
+                if 150 <= height <= 193:
+                    validTest += 1
+            elif cmin == "in":
+                if 59 <= height <= 76:
+                    validTest += 1
+            #print(hgtData)
+        hclData = hcltest.search(x)
+        if hclData:
+            hclData1 = hclData.groups()[1]
+            hexcode = re.search('(#)(([0-9a-f]){6})',hclData1)
+            if hexcode:
+                validTest += 1
+            #print(hclData)
+        eclData = ecltest.search(x)
+        if eclData:
+            eclData1 = eclData.groups()[1]
+            validEye = re.search('(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)',eclData1)
+            if validEye:
+                validTest += 1
+            #print(eclData1)
+        pidData = pidtest.search(x)
+        if pidData:
+            pidData1 = pidData.groups()[1]
+            validPid = re.search('^[0-9]{9}$',pidData1)
+            if validPid:
+                validTest += 1
+            #print(pidData)
+        cidData = cidtest.search(x)
+        if cidData:
+            cidData1 = cidData.groups()[1]
+            #validTest += 1
+            #print(cidData)
+    tmpArray = [byrData1, iyrData1, eyrData1, hgtData1, hclData1, eclData1, pidData1, cidData1, validTest]
+    if validTest == 7:
+        validVerify += 1
+    dataentry += 1
+    print(tmpArray)
+    byrData1 = ""
+    iyrData1 = ""
+    eyrData1 = ""
+    hgtData1 = ""
+    hclData1 = ""
+    eclData1 = ""
+    pidData1 = ""
+    cidData1 = ""
+    validTest = 0
+    dataArray.append(tmpArray)
+    #print(dataArray)
+    print(validVerify)
+    print(dataentry)
 
-def returnData(xcoord,ycoord,inputdata):
-    xcoord = xcoord - 1
-    ycoord = ycoord - 1
-    return inputdata[xcoord][ycoord]
 
-def treeshitt(treesx,treesy):
-    dataInput, sizex, sizey = data()
-    startcoordX = 1
-    startcoordY = 1
-    increasex = treesx
-    increasey = treesy
-    xCoOrd = startcoordX
-    yCoOrd = startcoordY
-    treeshit = 0
 
-    while xCoOrd < sizex + 1:
-        coordData = returnData(xCoOrd, yCoOrd, dataInput)
-        treeshit += coordData
-        xCoOrd += increasex
-        yCoOrd += increasey
-        if yCoOrd > sizey:
-            yCoOrd = yCoOrd - sizey
 
-    return treeshit
-
-print(11)
-ta=(treeshitt(1,1))
-print(ta)
-print(13)
-tb=(treeshitt(1,3))
-print(tb)
-print(15)
-tc=(treeshitt(1,5))
-print(tc)
-print(17)
-td=(treeshitt(1,7))
-print(td)
-print(21)
-te=(treeshitt(2,1))
-print(te)
-answer=ta*tb*tc*td*te
-print(answer)
+data()
