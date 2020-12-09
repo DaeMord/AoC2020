be2953696b2798f9ef622ed92be1f0923cf18386
daemord <daemord@gmail.com>
2020-12-09 14:34:28 +0000

    Day 09

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import math
 import time
+import itertools
 start_time = time.time()
 
 rex = re.compile('(...)\s(.)(\d*)')
@@ -8,64 +9,60 @@ rex = re.compile('(...)\s(.)(\d*)')
 def data():
 
     dataoutput = []
-    #with open('test08.txt') as f:
-    with open('input08.txt') as f:
+    #with open('test09.txt') as f:
+    with open('input09.txt') as f:
         for line in f:
             linedata = line.strip()
-            data = list(rex.search(linedata).groups())
-            dataoutput.append(data)
+            data = linedata
+            dataoutput.append(int(data))
 
     return dataoutput
 
-def arraysrc(data,src):
-    output = []
-    for y, i in enumerate(data):
-        if i[0] == src:
-            output.append(y)
+def somedata(data,varWide):
+    for loopValue, a in enumerate(data):
+        val = data[loopValue+varWide]
+        if val not in [sum(numbers) for numbers in itertools.combinations(data[loopValue:loopValue+varWide],2)]: return(val)
+
+def analyse(data,range):
+    invalid = []
+    for loopValue, a in enumerate(data):
+        checkValue = loopValue + range
+        dataTest = data[loopValue:checkValue]
+        dataToCheck = data[checkValue]
+        arrayToBeChecked = []
+        for i in dataTest:
+            for j in dataTest:
+                tmp = i + j
+                arrayToBeChecked.append(tmp)
+        if dataToCheck not in arrayToBeChecked:
+            invalid.append(dataToCheck)
+        if loopValue >= (len(data) - range - 1): break
+    return invalid[0]
+
+def analyse2(data,valueToFind):
+    i = 1
+    output = 0
+    for loopValue, a in enumerate(data):
+        while i < len(data):
+            dataSum = sum(data[loopValue:i])
+            if dataSum == valueToFind:
+                answerArray = data[loopValue:i]
+                if len(answerArray) > 1:
+                    output = min(answerArray) + max(answerArray)
+            if dataSum > valueToFind: break
+            i += 1
+        i = loopValue + 2
     return output
 
-def analyse(data):
-    i = 0
-    acc = 0
-    entrydone = [0]
-    while i < len(data):
-        if data[i][0] == "nop":
-            i += 1
-        elif data[i][0] == "jmp":
-            if data[i][1] == "+":
-                i = i + (int(data[i][2]))
-            elif data[i][1] == "-":
-                i = i - (int(data[i][2]))
-        elif data[i][0] == "acc":
-            if data[i][1] == "+":
-                acc += int(data[i][2])
-                i += 1
-            elif data[i][1] == "-":
-                acc -= int(data[i][2])
-                i += 1
-        if i not in entrydone:
-            entrydone.append(i)
-        else:
-            break
-    return(acc,i,len(data))
 
-def swptest(var1,var2):
-    datainput = data()
-    datahold = []
-    for i in arraysrc(datainput,var1):
-        datahold = datainput
-        datahold[i][0] = var2
-        x = analyse(datahold)
-        datahold[i][0] = var1
-        if (x[1]) == (x[2]):
-            return(i,x[0])
-
-answer1 = analyse(data())
-jmp = (swptest("jmp","nop"))
-nop = (swptest("nop","jmp"))
+wide = 25
+dataInput = data()
+answer1 = analyse(dataInput,wide)
+answer2 = analyse2(dataInput,answer1)
+print(somedata(dataInput,wide))
 print("Answer 1")
-print(answer1[0])
+print(answer1)
 print("Answer 2")
-print(jmp[1])
+print(answer2)
 
 print('Took', round(time.time() - start_time,2), 'seconds to complete')
