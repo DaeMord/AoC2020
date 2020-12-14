f0d62b3880021620dc04877e780a2d87d896ece2
daemord <daemord@gmail.com>
2020-12-14 13:51:18 +0000

    Day14

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 import re
-from AoC import rotate
 from AoC import inputData
 
 start_time = time.time()
@@ -9,79 +8,124 @@ start_time = time.time()
 #1 Actual live data
 dataSet = 1
 
+# 3459377600572 not answer
+
 if dataSet == 0:
-    dataInput = inputData('test13.txt')
+    dataInput = inputData('test14.txt')
 if dataSet == 1:
-    dataInput = inputData('input13.txt')
+    dataInput = inputData('input14.txt')
+if dataSet == 2:
+    dataInput = ['mask = 000000000000000000000000000000X1001X', 'mem[42] = 100']
+if dataSet == 3:
+    dataInput = inputData('test142.txt')
+if dataSet == 4:
+    dataInput = inputData('test143.txt')
+if dataSet == 5:
+    #dataInput = ['mask = X00X0100100X111X01001X10X001X00X0100', 'mem[363] = 58081970', 'mem[35105] = 31435010', 'mem[52446] = 4898641']
+    dataInput = ['mask = X00X0100100X111X01001X10X001X00X0100', 'mem[363] = 58081970']
+
+def maskData(mask):
+    output = []
+    for y, i in enumerate(mask [::-1]):
+        if i != 'X':
+            output.append((y,int(i)))
+    return output
+
+def merge(binary, mask):
+    binaryDict = dict(binary)
+    maskDict = dict(mask)
+    maxNum = binary + mask
+    maxNum = (max(map(max, maxNum)))
+    output = ''
+    for i in range(maxNum+1):
+        if maskDict.get(i) == None:
+            if binaryDict.get(i) == None:
+                output = output + '0'
+            else:
+                output = output + str(binaryDict.get(i))
+        else:
+            output = output + str(maskDict.get(i))
+    return output
+
+def merge2(binary, mask):
+    binaryDict = dict(binary)
+    maskDict = dict(mask)
+    output = []
+    maskcheck = ""
+    for i in range(36):
+        if maskDict.get(i) == None:
+            maskcheck = maskcheck + 'X'
+            if len(output) == 0:
+                output.append('0')
+                output.append('1')
+            else:
+                string0 = '0'
+                string1 = '1'
+                var0 = [x + string0 for x in output]
+                var1 = [x + string1 for x in output]
+                output = var0 + var1
+        else:
+            if maskDict.get(i) == 0:
+                maskcheck = maskcheck + '0'
+                if binaryDict.get(i) == None:
+                    string0 = '0'
+                else:
+                    string0 = str(binaryDict.get(i))
+            else:
+                maskcheck = maskcheck + '1'
+                string0 = '1'
+            if len(output) == 0:
+                output.append(string0)
+            else:
+                var0 = [x + string0 for x in output]
+                output = var0
+    actOutput = []
+    for i in output:
+        intOutput = int(i [::-1], base = 2)
+        actOutput.append(intOutput)
+    return actOutput
 
 def main():
-    busDepart = int(dataInput[0])
-    busTimes = dataInput[1].split(',')
-    dataBusTimesOriginal = []
-    dataBusTimesOutput = []
-    for i in busTimes:
-        if i != 'x':
-            dataBusTimesOriginal.append(int(i))
-            dataBusTimesOutput.append(int(i))
-    while min(dataBusTimesOutput) < busDepart:
-            minIndex = dataBusTimesOutput.index(min(dataBusTimesOutput))
-            dataBusTimesOutput[minIndex] = dataBusTimesOutput[minIndex] + dataBusTimesOriginal[minIndex]
-    minIndex = dataBusTimesOutput.index(min(dataBusTimesOutput))
-    return dataBusTimesOriginal[minIndex]*(dataBusTimesOutput[minIndex]-busDepart)
-
-def main2():
-    busDepart = int(dataInput[0])
-    busTimes = dataInput[1].split(',')
-    dataBusTimesOriginal = []
-    dataBusTimesOutput = []
-    diffIndex = []
-    countx = 1
-    for i in busTimes:
-        if i != 'x':
-            dataBusTimesOriginal.append(int(i))
-            dataBusTimesOutput.append(int(i))
-            diffIndex.append(int(countx))
-            countx = 1
-        elif i == 'x':
-            countx += 1
-    diffIndex.pop(0)
-    return function(dataBusTimesOriginal, diffIndex)
-
-def checkIfValid(val1, val2, diff):
-    origVal1 = val1
-    while True:
-        while (val1+diff) % val2 != 0:
-            val1 += origVal1
-        yield val1
-        val1 += origVal1
-
-def itterate(num1,num2,diffnum,reachNumber):
-    x = checkIfValid(num1, num2, diffnum)
-    while True:
-        y = next(x)
-        if y == reachNumber:
-            return y
-        if y > reachNumber:
-            return -1
-
-def function(array1, array2):
-    x = checkIfValid(array1[0], array1[-1], sum(array2))
-    i = -1
-    while True:
-        i += 1
-        numtocheck = next(x)
-        result = 0
-        y = -1
-        while result != -1:
-            y += 1
-            result = itterate(array1[y],array1[y+1],array2[y],numtocheck+sum(array2[:y]))
-            if y == len(array1)-2: break
-        if result > 0:
-            return numtocheck
+    memSize = 0
+    for data in dataInput:
+        if "mem" in data:
+            mem = re.search('mem[[](\d*)[]] = (\d*)', data).groups()
+            if int(mem[0]) > memSize:
+                memSize = int(mem[0])
+    #intitialise memory array
+    memArray1 = {}
+    memArray2 = {}
+    for data in dataInput:
+        if "mask" in data:
+            mask = re.search('mask = (\S*)',data)[1]
+            maskD = maskData(mask)
+        elif "mem" in data:
+            mem = re.search('mem[[](\d*)[]] = (\d*)',data).groups()
+            binaryMem = bin(int(mem[0]))
+            binary = bin(int(mem[1]))
+            binary = binary [::-1]
+            binaryMem = binaryMem[::-1]
+            binary = binary[:-2]
+            binaryMem = binaryMem[:-2]
+            binary = binary [::-1]
+            binaryMem = binaryMem[::-1]
+            binary = maskData(binary)
+            binaryMem = maskData(binaryMem)
+            finalBinary = merge(binary,maskD)
+            memAssign = merge2(binaryMem,maskD)
+            finalBinary = finalBinary [::-1]
+            val = int(finalBinary,base=2)
+            for i in memAssign:
+                memArray2[i] = int(mem[1])
+            memArray1[int(mem[0])-1] = val
+    return sum(memArray1.values()),sum(memArray2.values())
 
 print("Answer 1")
-print(main())
+print(main()[0])
 print("Answer 2")
-print(main2())
+answer2 = main()[1]
+if answer2 == 3459377600572:
+    print("Run it again")
+print(answer2)
 
 print('Took', round(time.time() - start_time,2), 'seconds to complete')
